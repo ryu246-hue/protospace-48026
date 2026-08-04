07563277697ba81aa535a61ff0318af2b653a337
ryu246-hue <oida1601@gmail.com>
2026-08-04 18:29:31 +0900

    Prepare for Render deployment

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :prototypes
+   devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+validates :name, presence: true
+validates :profile, presence: true
+validates :occupation, presence: true
+validates :position, presence: true
+end
